9039b5793ce6d172b5d15716174c5852920cc597
blueochild <blueochild@gmail.com>
2022-09-16 10:06:29 +0900

    Feat : MongoDB Connection

--- Foxrain-Messenger-Server/src/main.rs
+++ Foxrain-Messenger-Server/src/main.rs
@@ -1,8 +1,39 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+use std::env;
+use std::error::Error;
+use tokio;
+
+#[tokio::main]
+async fn database_connection() -> Result<(), Box<dyn Error>> {
+   // Load the MongoDB connection string from an environment variable:
+   let client_uri = "mongodb://localhost:27017";
+      //env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+   // A Client is needed to connect to MongoDB:
+   // An extra line of code to work around a DNS issue on Windows:
+   let options =
+      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+         .await?;
+   let client = Client::with_options(options)?;
+   let movies = client.database("local").collection::<T>("aga");
+
+   // Print the databases in our MongoDB cluster:
+   println!("Databases:");
+   for name in client.list_database_names(None, None).await? {
+      println!("- {}", name);
+   }
+   Ok(()) 
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("<h1>Hello world!</h1><br>test!!!")
+}
+
+#[get("/test/*")]
+async fn test() -> impl Responder {
+    HttpResponse::Ok().body("test!")
 }
 
 #[post("/echo")]
@@ -16,13 +47,16 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(test)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+
 }
