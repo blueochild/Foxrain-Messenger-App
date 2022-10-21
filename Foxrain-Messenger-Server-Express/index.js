b27bb069ea8a71ba422d17a4e45337de3dfcedfd
blueochild <blueochild@gmail.com>
2022-10-21 19:01:07 +0900

    New Feature : test

--- Foxrain-Messenger-Server-Express/index.js
+++ Foxrain-Messenger-Server-Express/index.js
@@ -12,7 +12,13 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
   console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
